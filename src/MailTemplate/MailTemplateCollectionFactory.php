6c6568646cebb3f96fbf2374ae193ba101078e34
Jan Matthiesen <jm@netinventors.de>
2025-03-14 11:55:57 +0100

    Cache require_once loaded php files

--- src/MailTemplate/MailTemplateCollectionFactory.php
+++ src/MailTemplate/MailTemplateCollectionFactory.php
@@ -8,18 +8,32 @@ use Symfony\Component\Filesystem\Path;
 
 class MailTemplateCollectionFactory
 {
+    /**
+     * @var array<string, MailTemplateCollection>
+     */
+    private static array $cache = [];
+
     public static function create(string $directory): MailTemplateCollection
     {
         $configurationFile = Path::join($directory, 'Resources/config/setup/mail-templates.php');
+
+        if (isset(self::$cache[$configurationFile])) {
+            return self::$cache[$configurationFile];
+        }
 
         /** @var array<class-string<MailTemplateInterface>, list<string>> $mailTemplatesConfiguration */
         $mailTemplatesConfiguration = [];
 
         if (\is_file($configurationFile)) {
-            /** @var array<class-string<MailTemplateInterface>, list<string>> $mailTemplatesConfiguration */
-            $mailTemplatesConfiguration = (array) require $configurationFile;
+            /**
+             * @noinspection UsingInclusionOnceReturnValueInspection
+             * @var array<class-string<MailTemplateInterface>, list<string>> $mailTemplatesConfiguration
+             */
+            $mailTemplatesConfiguration = (array) require_once $configurationFile;
         }
 
-        return new MailTemplateCollection($directory, $mailTemplatesConfiguration);
+        self::$cache[$configurationFile] = new MailTemplateCollection($directory, $mailTemplatesConfiguration);
+
+        return self::$cache[$configurationFile];
     }
 }
