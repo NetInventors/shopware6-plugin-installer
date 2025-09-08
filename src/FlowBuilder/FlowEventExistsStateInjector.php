7cbd64785b249c124f93a7edf6f912c89f7c0e1d
Jan Matthiesen <jm@netinventors.de>
2025-09-08 11:39:10 +0200

    Adds filter for flow event actions
    Adds a filter to the flow event search criteria, enabling filtering by event action names.
    
    This ensures that only flows with specific actions are retrieved,
    improving the accuracy and relevance of search results.

--- src/FlowBuilder/FlowEventExistsStateInjector.php
+++ src/FlowBuilder/FlowEventExistsStateInjector.php
@@ -30,6 +30,7 @@ final readonly class FlowEventExistsStateInjector
 
         $criteria
             ->addFilter(new EqualsAnyFilter('eventName', $installableFlows->getEventNames()))
+            ->addFilter(new EqualsAnyFilter('sequences.actionName', $installableFlows->getActionNames()))
         ;
 
         $flowEntities = $this->flowRepository->search($criteria, $context);
